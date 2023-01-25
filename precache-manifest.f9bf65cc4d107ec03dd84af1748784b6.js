1c8ca54f28be45e4d1930925e07aaa45e9408744
coolcapybara420 <michael_w_xu+cs1300@brown.edu>
2023-01-25 01:02:45 -0500

    Updates

---
+++ precache-manifest.f9bf65cc4d107ec03dd84af1748784b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cae633ced0585a406db2",
+    "url": "/redesign-capstone/static/css/main.43ac0f90.chunk.css"
+  },
+  {
+    "revision": "cae633ced0585a406db2",
+    "url": "/redesign-capstone/static/js/main.cae633ce.chunk.js"
+  },
+  {
+    "revision": "1a9932cdeed1b6058ba1",
+    "url": "/redesign-capstone/static/js/1.1a9932cd.chunk.js"
+  },
+  {
+    "revision": "97ba6cd5c69f586d446f",
+    "url": "/redesign-capstone/static/js/2.97ba6cd5.chunk.js"
+  },
+  {
+    "revision": "8f7abfd711013f97e9c4",
+    "url": "/redesign-capstone/static/js/runtime~main.8f7abfd7.js"
+  },
+  {
+    "revision": "04a939bf0661bdbbb4e7dd468876c613",
+    "url": "/redesign-capstone/static/media/jaswells-logo.04a939bf.png"
+  },
+  {
+    "revision": "4ce717dd4df3b81375e5f222250d6de3",
+    "url": "/redesign-capstone/static/media/jaswell-title-image.4ce717dd.jpeg"
+  },
+  {
+    "revision": "6068b9c965c3ee3c94821fe507b22ec5",
+    "url": "/redesign-capstone/static/media/jaswells1.6068b9c9.jpeg"
+  },
+  {
+    "revision": "9be4f40200509ffc6f57bfd3580a3587",
+    "url": "/redesign-capstone/static/media/Jaswells map.9be4f402.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/redesign-capstone/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "339438184b5500aa8af8eed936a43955",
+    "url": "/redesign-capstone/index.html"
+  }
+];
